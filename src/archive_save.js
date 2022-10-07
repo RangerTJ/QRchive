4b4f0e89d1b2fbb84aceaca5072715d31ad3e504
Raptor2k1 <87739732+Raptor2k1@users.noreply.github.com>
2022-10-06 18:06:46 -0700

    Update archive_save.js

--- src/archive_save.js
+++ src/archive_save.js
@@ -1,6 +1,10 @@
 /* 
 Date: 10/6/2022
-Description:  Helper method that takes a string (from the QR scanner results) and a description string (from the URL description ping helper method) and saves the URL,
-              description, and a timestamp entry to an archive JSON that can be accessed later to view previously scanned QR codes. Saves JSON locally.
+Description:  Helper method that takes a string (from the QR scanner results) and saves the URL, its description, and a timestamp entry to an archive JSON that 
+              can be accessed later to view previously scanned QR codes. Saves JSON locally. Calls helper method to get URL information.
 */
 
+//TO DO: Set up variable for URL
+//TO DO: Call for timestamp and define variable
+//TO DO: Call Description helper method
+//TO DO: Save/Open JSON with entry for values of: URL, Description, time
