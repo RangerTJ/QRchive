b5e5f11f7de0bb1d9f718fffb23483949d28859e
Raptor2k1 <87739732+Raptor2k1@users.noreply.github.com>
2022-10-06 18:07:53 -0700

    Update get_description.js

--- src/get_description.js
+++ src/get_description.js
@@ -3,3 +3,8 @@ Date: 10/6/2022
 Description:  Helper method that takes a string (from the QR scanner results) and pings the web address to get a description of the URL. If no description is available,
               or the ping times out, etc. it returns a generic placeholder description instead.
 */
+
+//TO DO: Define function
+//TO DO: Ping Server for Description
+//TO DO: Handle timeout/bad connection/invalid URL
+//TO DO: Return description from ping as string
